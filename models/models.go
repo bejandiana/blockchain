0b682e48d2b57976adea48c7dde23483e58bf069
Diana Bejan <bejan.diana.andrei@gmail.com>
2018-11-05 08:44:16 +0200

    changed the packages

--- models/models.go
+++ models/models.go
@@ -1,10 +1,9 @@
 package models
 
-import block "github.com/dianabejan/blockchain/bpm"
+import block "github.com/dianabejan/blockchain/block"
 
 type Blockchain interface {
 	CalculateHash() string
 	NextBlock(BPM int) (block.Block, error)
 	IsBlockValid(newBlock, oldBlock block.Block) bool
-	// ReplaceChain(newBlocks, Blockchain []block.Block) bool
 }
